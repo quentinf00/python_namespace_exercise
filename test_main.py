5e37e570616fe56c15aaae96b4de38d1bd367775
ClementWalter <clement0walter@gmail.com>
2018-09-20 14:46:14 +0200

    Use pytest to illustrate all situations

--- test_main.py
+++ test_main.py
@@ -1,10 +1,50 @@
-def set_up_mocks():
-    """All your code should go here, patch whatever you want (except the main.run method)
-     and try to get the correct output: good luck
-    """
-    pass
+from unittest.mock import patch
 
-if __name__ == '__main__':
-    import main
-    set_up_mocks()
+import main
+import package
+import package.module1
+from package import module1, MyClass
+
+
+@patch('package.CONSTANT')
+@patch('package.MyClass')
+@patch('package.my_function')
+def test_patch_package_should_patch_package_import(*args):
+    main.run()
+
+
+@patch.object(package, 'CONSTANT')
+@patch.object(package, 'MyClass')
+@patch.object(package, 'my_function')
+def test_patch_object_package_should_patch_package_import(*args):
+    main.run()
+
+
+@patch('package.module1.CONSTANT')
+@patch('package.module1.MyClass')
+@patch('package.module1.my_function')
+def test_patch_package_module_should_patch_module_import(*args):
+    main.run()
+
+
+@patch.object(package.module1, 'CONSTANT')
+@patch.object(package.module1, 'MyClass')
+@patch.object(package.module1, 'my_function')
+def test_patch_object_package_module_should_patch_module_import(*args):
+    main.run()
+
+
+@patch.object(module1, 'CONSTANT')
+@patch.object(module1, 'MyClass')
+@patch.object(module1, 'my_function')
+def test_patch_object_module_should_patch_module_import(*args):
+    main.run()
+
+
+@patch.object(MyClass, 'my_class_attribute')
+@patch.object(MyClass, 'this_should_stay_the_same')
+@patch.object(MyClass, 'my_class_method')
+@patch.object(MyClass, 'this_should_work_the_same')
+@patch.object(MyClass, 'my_class_static_method')
+def test_patch_object_my_class_should_patch_in_any_cases(*args):
     main.run()
